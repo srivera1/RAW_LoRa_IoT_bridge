4ccf36ecd9f9abfe1dd4a2eff7fc19b9a6ee3c3c
srivera1 <srivera@ucm.es>
2018-05-01 19:07:58 +0200

    Update RAW_lora.sh
    improving error detection

--- RAW_lora.sh
+++ RAW_lora.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 ## RAW_LoRa data logger
 ## Sergio Rivera Lavado
-
 
 stty -F /dev/ttyACM0 115200
 while true; do
   read -r f < /dev/ttyACM0
   u=$(date +%D/%H%M%S%N)
-  echo $u','$f >> RAW_lora_adquired
+  if [ $(echo $f | grep -o , | wc -l) -ge 5 ];
+    then echo $u','$f >> RAW_lora_adquired;
+  else sleep 1
+  fi
 done
